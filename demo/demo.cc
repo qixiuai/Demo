cc031ac795449d452d61759eda9f6d1edabbe8d6
braincodercn <braincoder@aliyun.com>
2019-04-04 16:50:18 +0800

    demo for boost

--- demo/demo.cc
+++ demo/demo.cc
@@ -1,6 +1,10 @@
 #include <iostream>
 
+#include "lib3.h"
+
 int main() {
+
+  std::cerr << get_lib3() << '\n';
   
   return 0;
 }
---
+++ demo/lib1.h
@@ -0,0 +1,11 @@
+
+#ifndef _LIB1_
+#define _LIB1_
+
+#include "boost/circular_buffer.hpp"
+
+int get_lib1() {
+  return 1;
+}
+
+#endif
