4f1873ec0915212599cddc2e7b30838af2fe2e99
braincodercn <braincoder@aliyun.com>
2019-04-08 13:50:27 +0800

    mimics PSGDeskop dependency

--- psg/event/lib2.h
+++ psg/event/lib2.h
@@ -1,7 +1,7 @@
 #ifndef _LIB2_
 #define _LIB2_
 
-#include "lib1.h"
+#include "psg/utils/lib1.h"
 #include "boost/circular_buffer.hpp"
 
 int get_lib2() {
--- psg/restapi/lib3.h
+++ psg/restapi/lib3.h
@@ -1,7 +1,7 @@
 #ifndef _LIB3_
 #define _LIB3_
 
-#include "lib2.h"
+#include "psg/event/lib2.h"
 #include "boost/circular_buffer.hpp"
 
 int get_lib3() {
